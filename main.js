84f5e7be79f50e9970d4426382f0de2256f8e5a1
Kuba Szóstko <kubaszostko@gmail.com>
2020-05-11 09:45:55 +0200

    added function for playing and stopping video

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-console.log('it works');
+const video = document.querySelector('.player__video');
+const playButton = document.querySelector('.play');
+const skipButtons = document.querySelectorAll('[data-skip]');
+const inputs = document.querySelectorAll('[type="range"]');
+
+function play() {
+    if(playButton.textContent === '►') {
+        video.play();
+        playButton.textContent = '❚ ❚';
+    } else {
+        video.pause();
+        playButton.textContent = '►';
+    }
+}
+
+playButton.addEventListener('click', play);
