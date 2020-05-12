0b3bc7b1275c1e61c9784192a0062bd66c1519d3
Kuba Szóstko <kubaszostko@gmail.com>
2020-05-12 17:06:15 +0200

    created function for changing video time via bar

--- main.js
+++ main.js
@@ -2,7 +2,10 @@ const video = document.querySelector('.player__video');
 const playButton = document.querySelector('.play');
 const skipButtons = document.querySelectorAll('[data-skip]');
 const inputs = document.querySelectorAll('[type="range"]');
+const bar = document.querySelector('.player__bar');
 const watchedBar = document.querySelector('.player__watched');
+
+let isClicked = false;
 
 function play() {
     if(playButton.textContent === '►') {
@@ -32,7 +35,24 @@ function handleChange(e) {
     video[e.target.dataset.property] = e.target.value;
 }
 
+function changeVideoTime(e) {
+    if(!isClicked) return;
+    
+    const { x } = video.getBoundingClientRect();
+    const videoPart = ((e.pageX - x) / video.offsetWidth) * 100;
+
+    watchedBar.style.flexBasis = `${videoPart}%`;
+    video.currentTime = (video.duration * videoPart) / 100;
+}
+
 playButton.addEventListener('click', play);
 video.addEventListener('timeupdate', updateBar);
+bar.addEventListener('mousedown', e => {
+    isClicked = true;
+    changeVideoTime(e);
+});
+bar.addEventListener('mouseup', () => isClicked = false);
+bar.addEventListener('mouseleave', () => isClicked = false);
+bar.addEventListener('mousemove', changeVideoTime);
 skipButtons.forEach(button => button.addEventListener('click', skip));
 inputs.forEach(input => input.addEventListener('input', handleChange));
