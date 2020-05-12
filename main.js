0027de2c1fd555a1274f59b400554f07f763e149
Kuba Szóstko <kubaszostko@gmail.com>
2020-05-12 16:19:37 +0200

    added function for updating bar

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const video = document.querySelector('.player__video');
 const playButton = document.querySelector('.play');
 const skipButtons = document.querySelectorAll('[data-skip]');
 const inputs = document.querySelectorAll('[type="range"]');
+const watchedBar = document.querySelector('.player__watched');
 
 function play() {
     if(playButton.textContent === '►') {
@@ -22,5 +23,11 @@ function skip(e) {
     }
 }
 
+function updateBar() {
+    const videoPart = (video.currentTime / video.duration) * 100;
+    watchedBar.style.flexBasis = `${videoPart}%`;
+}
+
 playButton.addEventListener('click', play);
-skipButtons.forEach(button => button.addEventListener('click', skip))
+skipButtons.forEach(button => button.addEventListener('click', skip));
+video.addEventListener('timeupdate', updateBar);
