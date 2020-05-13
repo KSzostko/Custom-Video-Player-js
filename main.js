9a8f9fb0d307b2cd0e584578deca14b96219e09e
Kuba Szóstko <kubaszostko@gmail.com>
2020-05-13 16:52:40 +0200

    added function for resizing video

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
+const videoPlayer = document.querySelector('.player');
 const video = document.querySelector('.player__video');
 const playButton = document.querySelector('.play');
+const resizeButton = document.querySelector('.resize');
 const skipButtons = document.querySelectorAll('[data-skip]');
 const inputs = document.querySelectorAll('[type="range"]');
 const bar = document.querySelector('.player__bar');
@@ -45,6 +47,24 @@ function changeVideoTime(e) {
     video.currentTime = (video.duration * videoPart) / 100;
 }
 
+function resizeVideo() {
+    const icon = resizeButton.querySelector('i');
+    video.style.height = '100%';
+    video.style.width = '100%';
+
+    if(document.fullscreenElement) {
+        icon.classList.replace('fa-compress', 'fa-expand');
+
+        document.exitFullscreen()
+            .catch(err => console.log('exitFullscreen failed', err));
+    } else {
+        icon.classList.replace('fa-expand', 'fa-compress');
+        
+        videoPlayer.requestFullscreen()
+            .catch(err => console.log('requestFullscreen failed', err));
+    }
+}
+
 playButton.addEventListener('click', play);
 video.addEventListener('timeupdate', updateBar);
 bar.addEventListener('mousedown', e => {
@@ -54,5 +74,6 @@ bar.addEventListener('mousedown', e => {
 bar.addEventListener('mouseup', () => isClicked = false);
 bar.addEventListener('mouseleave', () => isClicked = false);
 bar.addEventListener('mousemove', changeVideoTime);
+resizeButton.addEventListener('click', resizeVideo);
 skipButtons.forEach(button => button.addEventListener('click', skip));
 inputs.forEach(input => input.addEventListener('input', handleChange));
